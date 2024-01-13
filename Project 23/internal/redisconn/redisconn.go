5b237ca8bf5a941c5b0ee6d54ab45f7c35c483c2
Gromovoi Stanislav <gromila12@rambler.ru>
2024-01-13 11:29:27 +0600

    Project 23

--- Project 23/internal/redisconn/redisconn.go
+++ Project 23/internal/redisconn/redisconn.go
@@ -1,33 +1,39 @@
 package redisconn
 
 import (
-	"log"
 	"os"
+	"sync"
 
 	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
+
+var redisOnce sync.Once
+var redisInstance *redis.Client
+
 func GetRedisConnection() *redis.Client {
-
-    if err := godotenv.Load(); err != nil {
-		panic("Ошибка загрузки файла .env: " + err.Error())
-	}
-	redisURL := os.Getenv("REDIS_URL")
-
-	log.Println("GetRedisConnection")
-
-	if redisURL == "" {
-		return redis.NewClient(&redis.Options{
-			Addr:	  "localhost:6379",
-			Password: "",
-			DB:		  0,
-		})
-	} else {
-		opt, err := redis.ParseURL(redisURL)
-		if err != nil {
-			panic(err)
+	
+	redisOnce.Do(func() {
+		if err := godotenv.Load(); err != nil {
+			panic("Ошибка загрузки файла .env: " + err.Error())
 		}
-		return redis.NewClient(opt)
-	}
+		redisURL := os.Getenv("REDIS_URL")
+	
+		if redisURL == "" {
+			redisInstance = redis.NewClient(&redis.Options{
+				Addr:	  "localhost:6379",
+				Password: "",
+				DB:		  0,
+			})
+		} else {
+			opt, err := redis.ParseURL(redisURL)
+			if err != nil {
+				panic(err)
+			}
+			redisInstance= redis.NewClient(opt)
+		}
+	})
+
+    return redisInstance
 }
